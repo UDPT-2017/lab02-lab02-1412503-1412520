957dfc37928d5de61469636c08f73ac419da8b74
thikhin96 <nghoangthi1@gmail.com>
2017-05-14 01:17:01 +0700

    install some basic libraries and tools and organize folder family as a MVC model

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 var express = require('express');
 var app = express();
 var exphbs = require('express-handlebars');
+var path = require('path');
 
 app.use(express.static('public'));
 app.use('/components', express.static('bower_components'));
 
 app.engine('hbs', exphbs({}));
 app.set('view engine', 'hbs');
+//vì sẽ không tìm đc file views ngay cùng thư mục, nên phải cấu hình lại địa chỉ của file views để render
+app.set('views', path.resolve('.app/views'));
 
 var port = 3000;
 
