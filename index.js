4a530a681e05eade6e8bf321c091f831e404e118
thikhin96 <nghoangthi1@gmail.com>
2017-05-17 17:12:42 +0700

    add friendList.hbs

--- index.js
+++ index.js
@@ -30,6 +30,13 @@ app.get('/inbox', function(req, res) {
 	});
 })
 
+app.get('/friends', function(req, res) {
+	res.render('friendList', {
+		user: {name: 'thikhin96', userID: 31, email: 'thikhin96@yahoo.com', avatar: 'images/User.png'},
+		unreadMail: 4,
+		active_friendList: "active"
+	});
+})
 
 var port = 3000;
 
