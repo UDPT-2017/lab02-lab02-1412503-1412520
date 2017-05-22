a522bf4616e6f7fbcaf27dcacf56d5651f013e88
thaonguyen1096 <thaonguyen1096@gmail.com>
2017-05-22 22:40:52 +0700

    edit

--- app/controllers/messageController.js
+++ app/controllers/messageController.js
@@ -1,33 +1,22 @@
 var message = require('../models/message');
-
-//var userID = 2;
+//var userID = 1;
 
 var messageController = {
   new_mes: function(req, res){
     message.getAllFriend(function(err, allFriend){
-      if (req.user == null)
-      {
-        res.redirect('/login');
-      }
-      else{
-          res.render('new_mes', {
-          user: {name: req.user.name, userID: req.user.userid, email: req.user.email, avatar: req.user.avatar},
-          title: 'New Message',
-          message: 'Please fill details to send a new message',
-          recipent: allFriend,
-          active_messages: "active",
-          active_new_mes: "active",
-            }, req.user.userid);
-      } 
-    })
+      res.render('new_mes', {
+        user: req.user,
+        title: 'New Message',
+        message: 'Please fill details to send a new message',
+        unreadMail: 4,
+        recipent: allFriend,
+        active_messages: "active",
+        active_new_mes: "active",
+      });
+    },req.user.userid)
   },
   createMess: function(req, res){
-      if (req.user == null)
-      {
-        res.redirect('/login');
-      }
-      else{
-         var check = null;
+    var check = null;
     message.getAllFriend(function(err, allFr){
       for(var i=0;i<allFr.length;i++){
         if(allFr[i].email === req.body.recipent){
@@ -41,9 +30,10 @@ var messageController = {
             console.log(err);
             message.getAllFriend(function(err, allFriend){
               res.render('new_mes', {
-                 user: req.user,
+                user: req.user,
                 title: 'New Message',
                 message: 'Your message is not sent. Try again!!',
+                unreadMail: 4,
                 recipent: allFriend,
                 active_messages: "active",
                 active_new_mes: "active",
@@ -56,14 +46,15 @@ var messageController = {
                 user: req.user,
                 title: 'New Message',
                 message: 'Your message is sent.',
+                unreadMail: 4,
                 recipent: allFriend,
                 active_messages: "active",
                 active_new_mes: "active",
               });
-            },req.user)
+            },req.user.userid)
           }
         },{ content: req.body.content,
-            sender: req.user.userID,
+            sender: req.user.userid,
             recipent: req.body.recipent
           } )
       }
@@ -73,32 +64,28 @@ var messageController = {
             user: req.user,
             title: 'New Message',
             message: 'Your message is not sent. The recipent is not exit or not your friend. Try again!!',
+            unreadMail: 4,
             recipent: allFriend,
             active_messages: "active",
             active_new_mes: "active",
           });
         },req.user.userid)
       }
-    },userID)
-      }
+    },req.user.userid)
   },
   sent: function(req, res){
-     if (req.user == null)
-      {
-        res.redirect('/login');
-      }
-      else{
-        message.findAllMess(function(err, sent_message){
-        res.render('sent', {
-          user: req.user,
-          title: 'Sent',
-          sent_message: sent_message,
-          active_messages: "active",
-          active_sent: "active"
-        });
-        }, req.user.userid)
-      }
+    message.findAllMess(function(err, sent_message){
+      res.render('sent', {
+        user: req.user,
+        title: 'Sent',
+        unreadMail: 4,
+        sent_message: sent_message,
+        active_messages: "active",
+        active_sent: "active"
+      });
+    }, req.user.userid)
   }
 };
+
 
 module.exports = messageController;
